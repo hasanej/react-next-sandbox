64b511a24af4523787b20b2184babe69969eff8b
Hasan <hasan.el@accenture.com>
2023-05-20 06:43:48 +0700

    Update Register layout and page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
@@ -8,12 +8,10 @@ import * as Yup from 'yup';
 import { userService } from 'src/app/services';
 
 export default function Register() {
-  // form validation rules 
+  // Form validation rules 
   const validationSchema = Yup.object().shape({
-    firstName: Yup.string().required('First Name is required'),
-    lastName: Yup.string().required('Last Name is required'),
-    email: Yup.string().required('E-mail is required'),
-    password: Yup.string().required('Password is required')
+    email: Yup.string().required("E-mail is required"),
+    password: Yup.string().required("Password is required")
   });
   const formOptions = { resolver: yupResolver(validationSchema) };
 
@@ -28,8 +26,6 @@ export default function Register() {
       .then((res) => {
         setRegistrationFailed(false);
         setRegistrationMessage("Registration success, you can now proceed to Login page");
-        // Navigate to dashboard
-        // router.push("/dashboard");
       })
       .catch((error) => {
         setRegistrationFailed(true);
@@ -38,70 +34,66 @@ export default function Register() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col justify-center">
-      <div className="p-6 mx-auto bg-white rounded-xl shadow-lg flex-col space-x-4 w-1/3">
+    <div className='min-h-screen flex flex-col justify-center'>
+      <div className='p-6 mx-auto bg-white rounded-xl shadow-lg flex-col space-x-4 w-1/3'>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex-col min-w-full">
-            <div className="mb-2">
-              <p className="text-sky-500">First Name</p>
+          <div className='flex-col min-w-full'>
+            <div className='mb-2'>
+              <p className='text-sky-500'>First Name</p>
               <input
                 {...register('firstName')}
-                name="firstName"
-                type="text"
-                className={`form-control ${errors.firstName
-                  ? "is-invalid border-red-600 border rounded-md min-w-full text-black p-2"
-                  : "border-slate-500 border rounded-md min-w-full text-black p-2"}`
-                }
+                name='firstName'
+                type='text'
+                className={'border-slate-500 border rounded-md min-w-full text-black p-2'}
               />
-              <p className="text-red-500 text-sm">{errors.firstName?.message}</p>
             </div>
 
-            <div className="mb-2">
-              <p className="text-sky-500">Last Name</p>
+            <div className='mb-2'>
+              <p className='text-sky-500'>Last Name</p>
               <input
                 {...register('lastName')}
-                name="lastName"
-                type="text"
-                className={`form-control ${errors.lastName
-                  ? "is-invalid border-red-600 border rounded-md min-w-full text-black p-2"
-                  : "border-slate-500 border rounded-md min-w-full text-black p-2"}`
-                }
+                name='lastName'
+                type='text'
+                className={'border-slate-500 border rounded-md min-w-full text-black p-2'}
               />
-              <p className="text-red-500 text-sm">{errors.lastName?.message}</p>
             </div>
 
-            <div className="mb-2">
-              <p className="text-sky-500">E-mail</p>
+            <div className='mb-2'>
+              <p className={errors.email ? 'text-red-500' : 'text-sky-500'}>
+                E-mail
+              </p>
               <input
                 {...register('email')}
-                name="email"
-                type="email"
+                name='email'
+                type='email'
                 className={`form-control ${errors.email
-                  ? "is-invalid border-red-600 border rounded-md min-w-full text-black p-2"
-                  : "border-slate-500 border rounded-md min-w-full text-black p-2"}`
+                  ? 'is-invalid border-red-600 border rounded-md min-w-full text-black p-2'
+                  : 'border-slate-500 border rounded-md min-w-full text-black p-2'}`
                 }
               />
-              <p className="text-red-500 text-sm">{errors.email?.message}</p>
+              <p className='text-red-500 text-sm'>{errors.email?.message}</p>
             </div>
 
-            <div className="mb-2">
-              <p className="text-sky-500">Password</p>
+            <div className='mb-2'>
+              <p className={errors.password ? 'text-red-500' : 'text-sky-500'}>
+                Password
+              </p>
               <input
                 {...register('password')}
-                name="password"
-                type="password"
+                name='password'
+                type='password'
                 className={`form-control ${errors.password
-                  ? "is-invalid border-red-600 border rounded-md min-w-full text-black p-2"
-                  : "border-slate-500 border rounded-md min-w-full text-black p-2"}`
+                  ? 'is-invalid border-red-600 border rounded-md min-w-full text-black p-2'
+                  : 'border-slate-500 border rounded-md min-w-full text-black p-2'}`
                 }
               />
-              <p className="text-red-500 text-sm">{errors.password?.message}</p>
+              <p className='text-red-500 text-sm'>{errors.password?.message}</p>
             </div>
 
             {
               isRegistrationFailed !== null &&
-                <div className={isRegistrationFailed ? "mt-5 bg-rose-200 rounded-md py-2 px-5" : "mt-5 bg-green-200 rounded-md py-2 px-5"}>
-                  <p className={isRegistrationFailed ? "text-black text-center text-rose-800" : "text-black text-center text-green-800"}>
+                <div className={isRegistrationFailed ? 'mt-5 bg-rose-200 rounded-md py-2 px-5' : 'mt-5 bg-green-200 rounded-md py-2 px-5'}>
+                  <p className={isRegistrationFailed ? 'text-black text-center text-rose-800' : 'text-black text-center text-green-800'}>
                     {registrationMessage}
                   </p>
                 </div>
@@ -110,8 +102,8 @@ export default function Register() {
             <button
               disabled={formState.isSubmitting}
               className={formState.isSubmitting
-                ? "bg-zinc-400 min-w-full p-2 rounded-md mt-5"
-                : "bg-sky-500 min-w-full p-2 rounded-md mt-5"
+                ? 'bg-zinc-400 min-w-full p-2 rounded-md mt-5'
+                : 'bg-sky-500 min-w-full p-2 rounded-md mt-5'
               }
             >
               Register
@@ -119,10 +111,10 @@ export default function Register() {
           </div>
         </form>
 
-        <p className="text-black mt-2 text-center">Have account?
+        <p className='text-black mt-2 text-center'>Have account?
           <a
-            className="text-sky-500 ml-1"
-            href="/login">
+            className='text-sky-500 ml-1'
+            href='/login'>
               Login
           </a>
         </p>
