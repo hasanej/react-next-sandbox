9119d8be0ff875d3a33ccc63d55547b91d5abe45
Hasan <hasan.el@accenture.com>
2023-05-20 06:43:10 +0700

    Update header and footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ export { Footer };
 function Footer() {
   return (
     <div className="bg-sky-500 min-w-full min-h-fit absolute bottom-0 px-5 py-2 flex">
-      <div className="w-1/2">Footer Component</div>
+      <div className="w-1/2">Created by Hasan</div>
       <div className="text-end w-1/2">Accenture Song</div>
     </div>
   )
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,10 @@
 export { Header };
 
-function Header() {
+function Header({ headerTitle }) {
   return (
     <div className="bg-sky-500 min-w-full min-h-fit absolute top-0 px-5 py-4 flex">
       <a href="/">
-        <div className="min-w-full text-2xl">Welcome</div>
+        <div className="min-w-full text-2xl">{headerTitle}</div>
       </a>
     </div>
   )
